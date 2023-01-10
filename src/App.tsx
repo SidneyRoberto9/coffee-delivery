92306a8ac58ba1e542a967544609c609a54cecd8
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-10 10:49:59 -0300

    💄 style: Config default theme with styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-export default App
