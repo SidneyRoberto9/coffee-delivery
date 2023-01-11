04013dcd9b1b0c6259c73257ba6efe0b7d1b816a
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-11 19:06:40 -0300

    🐛 fix: Menu types and "irlandes" coffee add

--- src/@types/menu.d.ts
+++ src/@types/menu.d.ts
@@ -1,8 +1,34 @@
+export type Taste =
+  | 'Americano'
+  | 'Arabe'
+  | 'CafeComLeite'
+  | 'CafeGelado'
+  | 'Capuccino'
+  | 'ChocolateQuente'
+  | 'Cubano'
+  | 'Expresso'
+  | 'ExpressoCremoso'
+  | 'Havaiano'
+  | 'Latte'
+  | 'Macchiato'
+  | 'Mochaccino'
+  | 'Irlandes'
+
+export type CoffeeTag =
+  | 'Alcoólico'
+  | 'Tradicional'
+  | 'Especial'
+  | 'Gelado'
+  | 'Com Leite'
+
+export type MenuType = 'cart' | 'list'
+
 export interface Menu {
-  type: string
-  image: string
+  id?: number
+  type: MenuType
+  image: Taste
   title: string
   description: string
-  tags: string[]
+  tags: CoffeeTag[]
   price: number
 }
