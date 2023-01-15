b3f8dc1e4826383017afe340b44043005c56baea
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-14 23:39:19 -0300

    ✨ feat: style order page

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,9 +1,56 @@
-import { OrderContainer } from "./styles";
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+
+import orderLogo from "../../assets/orderLogo.png";
+import { Icon, OrderContainer, OrderMain } from "./styles";
 
 export function Order() {
   return (
     <OrderContainer>
-      <h1>Order</h1>
+      <header>
+        <strong>Uhu! Pedido confirmado</strong>
+        <p>Agora é so aguardar que logo o café chegará até você</p>
+      </header>
+
+      <OrderMain>
+        <div>
+          <span>
+            <Icon variant="purple">
+              <MapPin size={18} weight="fill" />
+            </Icon>
+            <div>
+              <p>
+                Entrega em <strong>Rua joão Daniel Martinelli, 102</strong>
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </span>
+
+          <span>
+            <Icon variant="yellow">
+              <Timer size={18} weight="fill" />
+            </Icon>
+            <div>
+              <p>Previsão de entrega</p>
+              <p>
+                <strong>20 min - 30 min</strong>
+              </p>
+            </div>
+          </span>
+
+          <span>
+            <Icon variant="orange">
+              <CurrencyDollar size={18} weight="fill" />
+            </Icon>
+            <div>
+              <p>Pagamento na entrega</p>
+              <p>
+                <strong>Cartão de Crédito</strong>
+              </p>
+            </div>
+          </span>
+        </div>
+        <img src={orderLogo} alt="" />
+      </OrderMain>
     </OrderContainer>
   )
 }
--- src/pages/Order/styles.ts
+++ src/pages/Order/styles.ts
@@ -1,3 +1,103 @@
 import styled from "styled-components";
 
-export const OrderContainer = styled.div``
+export const OrderContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: calc(100% - 20rem);
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  gap: 2.5rem;
+
+  header {
+    strong {
+      font-family: 'Baloo 2', cursive;
+      font-weight: 800;
+      font-size: 2rem;
+      line-height: 1.6;
+      color: ${({ theme }) => theme['yellow-500']};
+    }
+
+    p {
+      font-size: 1.25rem;
+      font-weight: 400;
+      color: ${({ theme }) => theme['gray-800']};
+    }
+  }
+`
+
+export const OrderMain = styled.main`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  p,
+  strong {
+    font-size: 1rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme['gray-700']};
+    line-height: 1.4;
+  }
+
+  strong {
+    font-weight: 700;
+  }
+
+  div:first-child {
+    width: 33rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 2.5rem;
+    gap: 2rem;
+
+    background: linear-gradient(
+          ${({ theme }) => theme.white},
+          ${({ theme }) => theme.white}
+        )
+        padding-box,
+      linear-gradient(
+          to right,
+          ${({ theme }) => theme['yellow-300']},
+          ${({ theme }) => theme['purple-300']}
+        )
+        border-box;
+    border-radius: 6px 36px;
+    border: 1px solid transparent;
+  }
+
+  span {
+    display: flex;
+    align-items: flex-start;
+    gap: 0.75rem;
+    div {
+    }
+  }
+`
+
+const ICON_VARIANT = {
+  purple: 'purple-300',
+  yellow: 'yellow-300',
+  orange: 'yellow-500',
+} as const
+
+interface IconProps {
+  variant: keyof typeof ICON_VARIANT
+}
+
+export const Icon = styled.ins<IconProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  border-radius: 999px;
+  padding: 0.5rem;
+  margin-top: 0.25rem;
+
+  background: ${({ theme, variant }) => theme[ICON_VARIANT[variant]]};
+  color: ${({ theme }) => theme.white};
+`
